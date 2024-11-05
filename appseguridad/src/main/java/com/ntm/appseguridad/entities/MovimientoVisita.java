5d645dbe0b9ab0c41c5ce333c80370d23b8a7a7f
Ignacio Coppede <nachocoppede@gmail.com>
2024-11-05 11:03:54 -0300

    Update MovimientoVisita.java

--- appseguridad/src/main/java/com/ntm/appseguridad/entities/MovimientoVisita.java
+++ appseguridad/src/main/java/com/ntm/appseguridad/entities/MovimientoVisita.java
@@ -1,14 +1,19 @@
-package com.ntm.appseguridad.business.domain;
+package com.ntm.appseguridad.entities;
 
+import com.ntm.appseguridad.entities.Base;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Entity
-@Data
 @NoArgsConstructor
+@Data
 @EqualsAndHashCode(callSuper=false)
 public class MovimientoVisita extends Base {
-
+    private Date fechaMovimiento;
+    private String observacion;
+    private String descripcionMovilidad;
 }
